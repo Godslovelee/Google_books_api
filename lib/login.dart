78680f7f8aa135be939b24861310082bfc120d1b
Godslovelee <godslovelee@yahoo.com>
2021-10-13 22:56:35 +0100

    "Stateful widget"

--- lib/login.dart
+++ lib/login.dart
@@ -2,3 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 enum FormInput { login, register }
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
