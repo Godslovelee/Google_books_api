4a7a8a86052e638a1991f323630c9db8e0dc739c
Godslovelee <godslovelee@yahoo.com>
2021-10-13 22:58:08 +0100

    "adding login inputs"

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,102 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
+
 enum FormInput { login, register }
+
+class LoginPage extends StatefulWidget {
+
+
+  LoginPage({this.authFireBase, this.onSignedIn});
+  final BaseAuthFireBase authFireBase;
+  final VoidCallback onSignedIn;
+
+  @override
+  _LoginPageState createState() => new _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
+  String _email;
+
+  String _password;
+
+  FormInput _formInput = FormInput.login;
+
+
+
+//Widget Tree
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Login Basic"),
+        ),
+        body: Container(
+            padding: EdgeInsets.all(16.0),
+            child: Form(
+              key: formKey,
+              child: Column(children: buildInputs() + buildSubmitButtons()),
+            )));
+  }
+
+  //Validative methods
+
+  List<Widget> buildInputs() {
+    return [
+      TextFormField(
+        decoration: InputDecoration(labelText: "email"),
+        //saving email and password state
+        onSaved: (value) => _email = value,
+        validator: (value) => value.isEmpty ? "Email Field is empty" : null,
+      ),
+      TextFormField(
+        onSaved: (data) => _password = data,
+        validator: (data) => data.isEmpty ? "Password cant be empty" : null,
+        decoration: InputDecoration(labelText: "password"),
+        obscureText: true,
+      ),
+      Padding(
+        padding: EdgeInsets.all(15),
+      )
+    ];
+  }
+
+  List<Widget> buildSubmitButtons() {
+    if (_formInput == FormInput.login) {
+      return [
+        FlatButton(
+            color: Colors.blueAccent,
+            child: Text(
+              "Login",
+              style: TextStyle(fontSize: 20.0),
+            ),
+            onPressed: () => validateAndSubmit()),
+        FlatButton(
+            color: Colors.blueAccent,
+            child: Text(
+              "Create an Account",
+              style: TextStyle(fontSize: 20.0),
+            ),
+            onPressed: () => navigateToRegister())
+      ];
+    } else {
+      return [
+        FlatButton(
+            color: Colors.blueAccent,
+            child: Text(
+              "Create an Account",
+              style: TextStyle(fontSize: 20.0),
+            ),
+            onPressed: () => validateAndSubmit()),
+        FlatButton(
+            color: Colors.blueAccent,
+            child: Text(
+              "Already have an Account? Login",
+              style: TextStyle(fontSize: 20.0),
+            ),
+            onPressed: () => navigateToLogin())
+      ];
+    }
+  }
+}
