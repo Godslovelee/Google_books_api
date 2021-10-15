841c75010db8091eebbf46a4b3b8170ebace51b5
Godslovelee <godslovelee@yahoo.com>
2021-10-15 23:23:54 +0100

    ""

--- lib/login.dart
+++ lib/login.dart
@@ -109,7 +109,7 @@ class _LoginPageState extends State<LoginPage> {
         FlatButton(
             color: Colors.blueAccent,
             child: Text(
-              "Create an Accoundsdst",
+              "Create an Accoundsddsdsst",
               style: TextStyle(fontSize: 20.0),
             ),
             onPressed: () => navigateToRegister())
