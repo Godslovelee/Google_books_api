82a904b1d32ee6e262c108fa03bab9f7b0cde6e9
Godslovelee <godslovelee@yahoo.com>
2021-10-15 23:17:43 +0100

    ""

--- lib/login.dart
+++ lib/login.dart
@@ -126,7 +126,7 @@ class _LoginPageState extends State<LoginPage> {
         FlatButton(
             color: Colors.blueAccent,
             child: Text(
-              "Already have an Account? Loddgdddin",
+              "Already have an Account? Login",
               style: TextStyle(fontSize: 20.0),
             ),
             onPressed: () => navigateToLogin())
