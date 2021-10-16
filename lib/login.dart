69c844a097b583751b1b8d8611ec5db256f226d4
Godslovelee <godslovelee@yahoo.com>
2021-10-16 23:35:59 +0100

    ""

--- lib/login.dart
+++ lib/login.dart
@@ -64,7 +64,7 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Google Bookfffs"),
+          title: Text("Google Book"),
         ),
         body: Container(
             padding: EdgeInsets.all(16.0),
