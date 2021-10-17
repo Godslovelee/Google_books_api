77fa81110c37c03f2ba5523fb43aa1cb3117f146
Godslovelee <godslovelee@yahoo.com>
2021-10-17 01:33:25 +0100

    "Login void callbacks"

--- lib/login.dart
+++ lib/login.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
+
 
 
 enum FormInput { login, register }
