32e4753d9255a787f78e71641396cd19ab3a2b9a
Godslovelee <godslovelee@yahoo.com>
2021-10-17 01:15:17 +0100

    "Abstract class for FireBaseAuth"

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,2 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'dart:async';
+
+abstract class BaseAuthFireBase{
+  Future<String>signInWithEmailAndPassword(email, password);
+  Future<String>createUserWithEmailAndPassword(email, password);
+  Future<String>currentUser();
+}
