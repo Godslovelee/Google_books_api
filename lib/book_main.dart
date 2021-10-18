0f244d3e29b9d646f0045d6cbe9d3c78cbd53d99
Godslovelee <godslovelee@yahoo.com>
2021-10-18 23:00:45 +0100

    "initializing stateful widget book_main.dart"

---
+++ lib/book_main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class BookMain extends StatefulWidget {
+  const BookMain({Key key}) : super(key: key);
+
+  @override
+  _BookMainState createState() => _BookMainState();
+}
+
+class _BookMainState extends State<BookMain> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
