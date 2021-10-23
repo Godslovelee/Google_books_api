044939ddde10d239863b90f466d41cb9bffdca19
Godslovelee <godslovelee@yahoo.com>
2021-10-24 00:07:05 +0100

    "favourite screen field variables"

---
+++ lib/favouriteScreen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import './data/bookshelper.dart';
+
+class FavouritesScreen extends StatefulWidget {
+  const FavouritesScreen({Key key}) : super(key: key);
+
+  @override
+  _FavouritesScreenState createState() => _FavouritesScreenState();
+}
+
+class _FavouritesScreenState extends State<FavouritesScreen> {
+ BooksHelper helper;
+List<dynamic> books = List<dynamic>();
+int boo
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
