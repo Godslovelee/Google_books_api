6173ff3f8f26f86fd0c6ded5501975d4c9fb6d17
Godslovelee <godslovelee@yahoo.com>
2021-10-28 23:46:18 +0100

    ""

--- lib/favouriteScreen.dart
+++ lib/favouriteScreen.dart
@@ -70,7 +70,7 @@ class _FavouritesScreenState extends State<FavouritesScreen> {
 
   Future initialize() async {
     books = await helper.getFavorites();
-    setState(() {dddddd
+    setState(() {dddddddd
       booksCount = books.length;
       books = books;
 
