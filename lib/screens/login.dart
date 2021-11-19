ca3e822f88a93a6508526492e9283a479aa084e4
Godslovelee <godslovelee@yahoo.com>
2021-11-19 22:04:01 +0100

    ""

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -44,7 +44,7 @@ class _LoginPageState extends State<LoginPage> {
         } else {
           String userID = await widget.authFireBase
               .createUserWithEmailAndPassword(_email, _password);
-          print("create-neffw-usffffer + $userID");
+          print("create-neffw-usfffffer + $userID");
         }
         widget.onSignedIn();
       } catch (e) {
